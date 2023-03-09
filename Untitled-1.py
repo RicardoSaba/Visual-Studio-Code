ea859b1aa4c638bff175ac67d38e750a6dd3a34b
Your Name <you@example.com>
2023-03-09 10:05:57 +0100

    Pokemon1

--- Untitled-1.py
+++
@@ -1,1 +0,0 @@
-print("Nafri Jero")
--- main.py
+++
@@ -1,6 +0,0 @@
-i = input('Was soll wiederholt werden? ')
-a = int(input('Wie oft? ')) 
-for k in range(a):
-    print(i)
-
-#Marcel :D das habe ich selber gemacht!
